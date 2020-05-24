1c5794b635a00b87436e18eeaaec4b75b6a4c12b
Tira-chan desu~ <yeonickyc@gmail.com>
2020-05-24 09:33:55 +0700

    Update play.js

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -1,1 +1,33 @@
+const { Command } = require("discord-akairo")
 
+module.exports = class PlayCommand extends Command {
+  constructor(){
+    super("play", {
+      aliases: ["Music"],
+      description: {
+        content: "Play music",
+        usage: "<query>",
+        examples: [
+          "Counting stars",
+          "Client",
+          "https://youtube.com/watch?v=XxXxXxXxXxX"
+        ]
+      },
+      category: "Music",
+    })
+  }
+  
+  args() {
+    const query = yield {
+      match: "content",
+      prompt: {
+        start: (msg, text) =>
+          `${msg.author}, what would you like to play music?`
+      }
+    };
+    
+    return { query }
+  }
+  
+  async exec(message, { query })
+}
